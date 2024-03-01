1ec66c7346f76ec3b0672618a2e2c6ad613b7918
Shahmar Aghayev <sahmaraqayev32@gmail.com>
2024-03-01 17:18:57 +0400

    first commit

---
+++ Api/Program.cs
@@ -0,0 +1,52 @@
+using Configuration;
+using Database.DbContexts;
+using Microsoft.OpenApi.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policyBuilder => policyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }); });
+
+var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
+if (mongoDbSettings is not null) builder.Services.AddSingleton(new MongoDbContext(mongoDbSettings));
+
+// builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+// builder.Services.AddScoped<IApartmentService, ApartmentService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1"));
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.Run();
